f78f9ca0aab2097a60a23723e9b078989276d6a9
bexruzbekgofurboyev-lgtm <bexruzbekgofurboyev@gmail.com>
2026-04-18 23:33:31 +0500

    Add files via upload

---
+++ sec_2.py
@@ -0,0 +1,62 @@
+print("Hello, World!")
+def count(n):
+    for i in range(n):
+        print(i)
+def gip(a,b):
+    return (a**2 + b**2)**0.5
+
+if __name__=="__main__":
+    def harf(s):
+        #global f
+        f="Rayxon"
+        print(f)
+        for i in s:
+            print(i)
+    gip(3,4)
+f="Bekhruzbek"
+print(int(gip(3,4)))
+print(f)
+def balance(balanc):
+    print(f"Your balance is: {balanc}")
+
+def deposit(balanc):
+    depo=float(input("How much do you want to deposit: "))
+    if depo>0:
+        print("--Deposit successfully--")
+        return depo
+    else:
+        print("--Only positive number--")
+        return 0
+def withdraw(balanc):
+    withdraw=float(input("How much do you want withdraw:"))
+
+    if withdraw>0 and withdraw<balanc:
+        print("--Withdraw successfully--")
+        return withdraw
+    else:
+        print("--Balance insuficient--")
+        return 0
+
+def main():
+    again=True
+    balanc=10.0
+    while again:
+        print("-"*20)
+        print("1. Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+        choice=int(input("Biror amaliyotni tanlang: "))
+        if choice==1:
+            balance(balanc)
+        elif choice==2:
+            balanc+=deposit(balanc)
+        elif choice==3:
+            balanc-=withdraw(balanc)
+        elif choice==4:
+            again=False
+        else:
+            print("Mavjud bo'lmagan amaliyot :(")
+    print("alvida!")
+if __name__=="__main__":
+    main()
